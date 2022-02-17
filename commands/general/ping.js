cdf576bce2653545737ef03a1f935d8f9ed32a46
meesam4687 <meesam4687@gmail.com>
2022-02-17 20:23:36 +0530

    Removed Ping Command and Added Status Command

--- commands/general/ping.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-    name: "ping",
-    run: async (client, message, args) => {
-      message.reply(`🏓 Pong! Bot Ping Is ${client.ws.ping}ms`).catch(console.error);
-    }
-}
---
+++ commands/general/status.js
@@ -0,0 +1,14 @@
+module.exports = {
+    name: "status",
+    run: async (client, message, args) => {
+        const Discord = require('discord.js');
+        let embed = new Discord.MessageEmbed()
+            .setColor('RANDOM')
+            .setTitle('Status')
+            .setThumbnail(client.user.avatarURL())
+            .addField('Ping', client.ws.ping, true)
+            .addField('Uptime', client.uptime, true)
+            .setTimestamp();
+        message.channel.send(embed);
+    }
+}
