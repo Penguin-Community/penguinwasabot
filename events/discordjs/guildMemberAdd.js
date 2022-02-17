171392431502c907ae1b0c2e3004979ddf2f4e9a
Meesam <70945820+meesam4687@users.noreply.github.com>
2022-02-17 20:14:55 +0530

    Update guildMemberAdd.js

--- events/discordjs/guildMemberAdd.js
+++ events/discordjs/guildMemberAdd.js
@@ -7,7 +7,7 @@ module.exports = {
     if(member.guild.id !== '905388276356104192') return;
     member.send('Welcome to Penguin Community!').catch(console.error)
     await wait(1000)
-    member.send({ content: "If you are stuck then you can check `#verify` channel and react (click the thing encircled in the screenshot below)", files: ['../penguin/things/e.png'] }).catch(console.error)
+    member.send({ content: "If you are stuck then you can check `#verify` channel and react (click the thing encircled in the screenshot below)", files: ['./e.png'] }).catch(console.error)
     await wait(1000)
     member.send('Hope you have a good time!').catch(console.error)
   },
