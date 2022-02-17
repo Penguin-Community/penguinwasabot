a1e7dc6bcd7284185461d6cd0cb58d1a470bc23b
meesam4687 <meesam4687@gmail.com>
2022-02-17 20:26:30 +0530

    Added Status Command

--- commands/general/status.js
+++ commands/general/status.js
@@ -9,6 +9,6 @@ module.exports = {
             .addField('Ping', client.ws.ping, true)
             .addField('Uptime', client.uptime, true)
             .setTimestamp();
-        message.channel.send(embed);
+        message.channel.send({embedds: [embed]});
     }
 }
