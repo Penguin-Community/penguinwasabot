20a7a8cf0dad1ef5b98aaef90a0a38c62186cfe7
Meesam <70945820+meesam4687@users.noreply.github.com>
2022-02-18 15:18:01 +0530

    Restricted the Bot to Penguin Community

--- events/discordjs/messageCreate.js
+++ events/discordjs/messageCreate.js
@@ -2,6 +2,7 @@ const path = require("path")
 module.exports = {
   name: "messageCreate",
   async execute(message){
+    let allowed_servers = ["765125984323633161", "905388276356104192"];
     const client = message.client
     const customprefix = await client.db.get(`customprefix_${message.author.id}`)
     if(message.content === "pw!getprefix"){
@@ -15,6 +16,7 @@ module.exports = {
     const command = args.shift().toLowerCase();
     const cmd = client.commands.get(command) || client.commands.get(client.aliases.get(command));
     if (!cmd) return;
+    if(!allowed_servers.includes(message.guild.id)) return message.channel.send("This bot has been restricted to Penguin Community. \n Join Penguin Community if you want to use this bot. \nhttps://discord.gg/cJ29fa2ZQR");
     try {
       cmd.run(client, message, args)
     } catch (e) {
