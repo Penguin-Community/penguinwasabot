78bf7572700393293940d8df81ea22d8d63344a9
Meesam <70945820+meesam4687@users.noreply.github.com>
2022-03-04 20:48:38 +0530

    Update ai.js

--- events/discordjs/ai.js
+++ events/discordjs/ai.js
@@ -3,7 +3,7 @@ module.exports = {
   name: "messageCreate",
   async execute(message){
     if(!message.mentions.has(message.client.user)) return;
-    await fetch("https://api.brainshop.ai/get?bid=164412&key=${process.env.AIKEY}&uid=!&msg=${encodeURIComponent(message.content)}")
+    await fetch(`https://api.brainshop.ai/get?bid=164412&key=${process.env.AIKEY}&uid=!&msg=${encodeURIComponent(message.content)}`)
       .then((response) => {
         response.json().then((result) => {
           message.reply(result.cnt)
