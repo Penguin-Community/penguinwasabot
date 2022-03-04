29cea0f5da63d9cdcab170f1f4468f364317f8ee
Meesam <70945820+meesam4687@users.noreply.github.com>
2022-03-04 20:40:55 +0530

    Create ai.js

---
+++ events/discordjs/ai.js
@@ -0,0 +1,13 @@
+const fetch = require("node-fetch-commonjs")
+module.exports = {
+  name: "messageCreate",
+  async execute(message){
+    if(!message.mentions.has(message.client.user)) return;
+    await fetch("https://api.brainshop.ai/get?bid=164412&key=${process.env.AIKEY}&uid=!&msg=${encodeURIComponent(message.content)}")
+      .then((response) => {
+        response.json().then((result) => {
+          message.reply(result.cnt)
+        }
+     }
+  },
+}
