db3376a9d822f9efbe11e25502e22d27c6d9ad52
meesam4687 <meesam4687@gmail.com>
2022-03-05 14:23:39 +0530

    AI Modified

--- events/discordjs/ai.js
+++ events/discordjs/ai.js
@@ -5,25 +5,6 @@ module.exports = {
     if(!message.mentions.has(message.client.user)) return;
     let msg = message.content.replace(/(<@!\d{18}>|<@\d{18}>)/g, " ")
     if(msg === " ") return message.reply("?");
-    let args = msg.split(" ");
-    if(args.includes("who")){
-      if(args.includes("created") && args.includes("you")){
-        message.reply("I was created by Meesamᴾᴺᴳ#3751. AI Powered by Acobot")
-        return;
-      }
-      if(args.includes("is") && message.content.includes("your") && args.includes("creator")){
-        message.reply("I was created by Meesamᴾᴺᴳ#3751. AI Powered by Acobot")
-        return;
-      }
-      if(args.includes("created") && args.includes("you?")){
-        message.reply("I was created by Meesamᴾᴺᴳ#3751. AI Powered by Acobot")
-        return;
-      }
-      if(args.includes("is") && message.content.includes("your") && args.includes("creator?")){
-        message.reply("I was created by Meesamᴾᴺᴳ#3751. AI Powered by Acobot")
-        return;
-      }
-    }
     await fetch(`http://api.brainshop.ai/get?bid=164412&key=3XOdwDh5edlCzoMB&uid=1&msg=${encodeURIComponent(msg)}`)
       .then((response) => {
         response.json().then((result) => {
