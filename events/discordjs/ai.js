246a910dcf6318c106d0080de32628653f99b6ef
meesam4687 <meesam4687@gmail.com>
2022-03-05 14:15:29 +0530

    e

--- events/discordjs/ai.js
+++ events/discordjs/ai.js
@@ -15,6 +15,14 @@ module.exports = {
         message.reply("I was created by Meesamᴾᴺᴳ#3751. AI Powered by Acobot")
         return;
       }
+      if(args.includes("created") && args.includes("you?")){
+        message.reply("I was created by Meesamᴾᴺᴳ#3751. AI Powered by Acobot")
+        return;
+      }
+      if(args.includes("is") && message.content.includes("your") && args.includes("creator?")){
+        message.reply("I was created by Meesamᴾᴺᴳ#3751. AI Powered by Acobot")
+        return;
+      }
     }
     await fetch(`http://api.brainshop.ai/get?bid=164412&key=3XOdwDh5edlCzoMB&uid=1&msg=${encodeURIComponent(msg)}`)
       .then((response) => {
